94e051b1c848e204bdd2883472b2771b9bfa77c4
Aaryan Dhand <aaryandhand@gmail.com>
2024-03-06 15:26:00 -0700

    up to 3.6.2

--- src/index.js
+++ src/index.js
@@ -21,12 +21,15 @@ class Board extends React.Component {
   
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
       xIsNext: !this.state.xIsNext,
     });
-}
+  }
   
     renderSquare(i) {
       return (
@@ -38,10 +41,15 @@ class Board extends React.Component {
     }
   
     render() {
-      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-  
-      
-      return (
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+          status = 'Winner: ' + winner;
+        } else {
+          status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+    
+        return (
         <div>
           <div className="status">{status}</div>
           <div className="board-row">
@@ -63,7 +71,17 @@ class Board extends React.Component {
       );
     }
   }
-  class Game extends React.Component {
+class Game extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        history: [{
+          squares: Array(9).fill(null),
+        }],
+        xIsNext: true,
+      };
+    }
+  
     render() {
       return (
         <div className="game">
@@ -78,7 +96,27 @@ class Board extends React.Component {
       );
     }
   }
-  
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
   // ========================================
   
   const root = ReactDOM.createRoot(document.getElementById("root"));
